f7cca7d823ff6186c31e87384871bf68d5f8f503
Salman Ahmed <mrwoow065@gmail.com>
2026-07-20 16:52:44 +0500

    practice tasks

---
+++ json.js
@@ -0,0 +1,28 @@
+const book={
+    title:"The Great Gatsby",
+    author:"F. Scott Fitzgerald",
+    price:10.99
+}
+console.log(book.title);
+
+
+const students=[
+    {
+        name:"raza",
+        age:20,
+        city:"lahore"
+    },
+    {
+        name:"ali",
+        age:22,
+        city:"karachi"
+    },
+    {
+        name:"ahmed",
+        age:21,
+        city:"islamabad"
+    }
+    
+]
+console.log(students);
+console.log(students[2].city)
---
+++ practice.map.js
@@ -0,0 +1,10 @@
+const number=[1,2,5,6,8,9];
+const show= number.map(num=> num*2);
+console.log(show);
+
+
+const name=["saLman", "uMer", "alI"]
+const showName= name.map(name=> name.toUpperCase());
+const showName2=name.map(name=> name.toLowerCase());
+console.log(showName);
+console.log(showName2);
